f80f5c533da292033e06fbd0425578afd122e8e1
bhtz <heintz.benjamin@gmail.com>
2015-03-15 22:22:47 +0100

    add samples

--- src/Hub.js
+++ src/Hub.js
@@ -3,8 +3,41 @@
 // Hub class.
 class Hub {
 
-	constructor(){
-		
+	constructor(namespace, io, socket){
+		this.namespace = namespace;
+		this.io        = io;
+		this.socket    = socket;
+		this._parseRoute();
+	}
+
+	static namespace(){
+		return '/';
+	}
+
+	get routes(){
+		return {};
+	}
+
+	_parseRoute(){
+		for(let route in this.routes){
+			this.socket.on(route, this.routes[route].bind(this));
+		}
+	}
+
+	emit(key, obj) {
+		this.socket.emit(key, obj);
+	}
+
+	emitToRoom(room, key, obj) {
+		this.socket.to(room).emit(key, obj);
+	}
+
+	broadcast(key, obj) {
+		this.io.of(this.namespace).emit(key, obj);
+	}
+
+	broadcastToAll(key, obj) {
+		this.io.sockets.emit(key, obj);
 	}
 }
 
--- src/WsApplication.js
+++ src/WsApplication.js
@@ -1,22 +1,43 @@
 // Imports
+var IO = require('socket.io');
 
 // WebSocket application.
 class WsApplication {
 
-	constructor(){
+	// constructor
+	// options
+	constructor(httpServer){
+		if(!httpServer){ 
+			throw new Error('WsApplication require constructor with http server object');
+		}
 
+		this.server = httpServer;
+		this.io = IO(this.server);
+		this._registerHubs();
 	}
 
+	// return list of hubs
 	get hubs(){
 		return [];
 	}
 
-	onConnection(){
-
+	// register WsApplication hub
+	_registerHubs(){
+		for(let Hub of this.hubs){
+			this.io.of(Hub.namespace()).on('connection', (socket) => {
+				this.onConnection(Hub.namespace());
+				new Hub(Hub.namespace(), this.io, socket);
+				socket.on('disconnect', this.onDisconnect.bind(this, Hub.namespace()));
+			});
+		}
 	}
 
-	onDisconnect(){
+	onConnection(namespace){
+		console.log('user connected to namespace : ' + namespace);
+	}
 
+	onDisconnect(namespace){
+		console.log('user disconnected from namespace : ' + namespace);
 	}
 }
 
